a800487d066769de5a63f43add89030fb6e82cb0
Halin <d.halin.lee@gmail.com>
2016-08-12 17:47:47 +0800

    1.调整看门狗

--- TestApp/fundamental/src/main/java/me/halin/fundamental/Tools/WatchDogService.java
+++ TestApp/fundamental/src/main/java/me/halin/fundamental/Tools/WatchDogService.java
@@ -1,6 +1,5 @@
 package me.halin.fundamental.Tools;
 
-import android.app.Application;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Printer;
@@ -8,6 +7,7 @@ import android.util.Printer;
 import java.util.Arrays;
 
 import me.halin.fundamental.LogUtil.Logger;
+
 
 /**
  * Created by Halin on 8/12/16.
@@ -31,6 +31,10 @@ public class WatchDogService implements Printer, Runnable {
     private Looper watchDogLooper;
     private Handler watchDogHandler;
     private boolean isInMessage = false;
+    private StackTraceElement[] heavyMessageStack = null;
+    private long messageStartTime;
+
+
     /**
      * 事件超时时间
      */
@@ -53,6 +57,7 @@ public class WatchDogService implements Printer, Runnable {
 
             Looper.getMainLooper().setMessageLogging(this);
             Logger.log(TAG, "看门狗程序初始化完成");
+
         }
     }
 
@@ -62,29 +67,49 @@ public class WatchDogService implements Printer, Runnable {
         if (x.contains("Dispatching")) {
             if (isInMessage) {
                 Logger.logE(TAG, "连续收到两次进入 Message 事件");
+                quit();
                 return;
             }
             isInMessage = true;
-            watchDogHandler.removeCallbacks(this);
-            watchDogHandler.postDelayed(this, timeout);
-            if (DEBUG)
-                Logger.log(TAG, "进入 Message");
+            onDispatchMessage();
         } else if (x.contains("Finished")) {
             isInMessage = false;
-            watchDogHandler.removeCallbacks(this);
-            if (DEBUG)
-                Logger.log(TAG, "退出 Message");
+            onFinishedMessage();
         } else {
             Logger.logE(TAG, "收到未能识别的事件");
             quit();
         }
     }
 
+
     @Override
     public void run() {
-        String stack = Arrays.toString(ThreadTools.getMainThread().getStackTrace());
-        Logger.logE(TAG, "超过 %d 秒主线程无响应,堆栈:%s", timeout, stack);
+        //Looper执行超时,记录堆栈
+        heavyMessageStack = ThreadTools.getMainThread().getStackTrace();
     }
+
+    private void onDispatchMessage() {
+        messageStartTime = System.currentTimeMillis();
+        watchDogHandler.removeCallbacks(this);
+        watchDogHandler.postDelayed(this, timeout);
+
+        if (DEBUG)
+            Logger.log(TAG, "进入 Message");
+    }
+
+
+    private void onFinishedMessage() {
+        watchDogHandler.removeCallbacks(this);
+        if (heavyMessageStack != null) {
+            String stack = Arrays.toString(heavyMessageStack);
+            Logger.logE(TAG, "主线程超时 用时:%d,堆栈:%s", System.currentTimeMillis() - messageStartTime, stack);
+            heavyMessageStack = null;
+        }
+        if (DEBUG)
+            Logger.log(TAG, "退出 Message");
+
+    }
+
 
     public void quit() {
         Looper.getMainLooper().setMessageLogging(null);
@@ -99,4 +124,6 @@ public class WatchDogService implements Printer, Runnable {
     public void setTimeout(long timeout) {
         this.timeout = timeout;
     }
+
+
 }
