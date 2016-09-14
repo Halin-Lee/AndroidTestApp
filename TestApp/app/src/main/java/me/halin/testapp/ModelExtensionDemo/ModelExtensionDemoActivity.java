8caa0573d3d967f67aa2eafd25d9b1de089b1f70
Halin <d.halin.lee@gmail.com>
2016-09-14 15:16:50 +0800

    1.JNI demo 2.model extension修复

--- TestApp/app/src/main/java/me/halin/testapp/ModelExtensionDemo/ModelExtensionDemoActivity.java
+++ TestApp/app/src/main/java/me/halin/testapp/ModelExtensionDemo/ModelExtensionDemoActivity.java
@@ -1,8 +1,13 @@
 package me.halin.testapp.ModelExtensionDemo;
 
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import me.halin.testapp.R;
+import me.halin.testapp.databinding.ActivityModelExtensionDemoBinding;
 
 /**
  * Created by Halin on 9/14/16.
@@ -12,5 +17,12 @@ public class ModelExtensionDemoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ActivityModelExtensionDemoBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_model_extension_demo);
+        ModelExtensionDBModel dbModel = new ModelExtensionDBModel();
+        dbModel.setAge(11);
+        ModelExtensionUIModel uiModel = new ModelExtensionUIModel(dbModel);
+        binding.setUiModel(uiModel);
+
     }
 }
