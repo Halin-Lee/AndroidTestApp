0fa8d3aed421adcab88400c41838a284f3df6c3a
Halin <d.halin.lee@gmail.com>
2017-03-16 16:03:48 +0800

    1.整理部分遗漏的包命名 2.添加TextInputLayout demo

--- TestApp/androiduidemo/src/main/java/me/halin/android/ui/coordinatorlayout/CoordinatorLayoutSnackbarActivity.java
+++ TestApp/androiduidemo/src/main/java/me/halin/android/ui/coordinatorlayout/CoordinatorLayoutSnackbarActivity.java
@@ -6,7 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-import me.halin.android.ui.R;
+import me.halin.android.R;
+
 
 public class CoordinatorLayoutSnackbarActivity extends AppCompatActivity {
 
--- TestApp/androiduidemo/src/main/java/me/halin/android/ui/drawerlayout/DrawerLayoutActivity.java
+++ TestApp/androiduidemo/src/main/java/me/halin/android/ui/drawerlayout/DrawerLayoutActivity.java
@@ -9,7 +9,8 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-import me.halin.android.ui.R;
+import me.halin.android.R;
+
 
 public class DrawerLayoutActivity extends AppCompatActivity {
 
